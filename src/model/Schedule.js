4b84db2c620ebc09fc33236d02e0aec360c03973
EC2 Default User <ec2-user@ip-172-31-86-9.ec2.internal>
2018-03-11 20:26:22 +0000

    Added Schedule query js file

---
+++ src/model/Schedule.js
@@ -0,0 +1,29 @@
+/*************************************
+Matthew Anderson and Christopher Piemonte
+CS 340 - Final Project
+March 10, 2018
+*************************************/
+
+const db = require('../dbcon');
+
+function Schedule(){}
+
+Schedule.prototype.getAll = function(callback) {
+    var query = "SELECT e.name AS eventName, a.first_name AS firstName, a.last_name AS lastName, a.id AS athleteId, c.name AS countryName, ci.event_date AS eventDate, l.name AS eventLocation FROM athletes a " +
+	"INNER JOIN countries c ON (a.country_id = c.id) " +
+	"INNER JOIN competes_in ci ON (a.id = ci.aid) " +
+    "INNER JOIN events e ON (ci.eid = e.id) " +
+    "INNER JOIN locations l ON (e.location_id = l.id) " +
+    "ORDER BY eventDate DESC;";
+    
+    db.query(query, function(err, results){
+        if(err) {
+            console.log("An err occured getting all events: " + err);
+            return callback(err);
+        }
+        callback(null, results);
+    });
+};
+
+
+module.exports = new Schedule();
